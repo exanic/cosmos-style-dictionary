7dc5b1232e0f3e8cc0fe99b94999d7e55a42affe
Sadwick Schuldt <sadwick.schuldt@exanic.ch>
2021-06-28 17:45:32 +0200

    Runable Build Script with Custom Formats / Transforms for json

--- build.js
+++ build.js
@@ -1,19 +1,60 @@
-const StyleDictionary = require('style-dictionary').extend({
-    source: [
-        "01_Global/*.json",
-        "02_Theme/*.json"
-    ],
-    platforms: {
-        scss: {
-            transformGroup: 'scss',
-            buildPath: 'build/scss/material-design/',
-            files: [{
-                destination: '_variables.scss',
-                format: 'scss/variables'
-            }]
+const StyleDictionaryPackage = require('style-dictionary');
+
+// CONFIG
+function getStyleDictionaryConfig() {
+    return {
+        "source": [
+            "01_Global/*.json",
+            "02_Theme/*.json"
+        ],
+        "platforms": {
+            "scss": {
+                "transformGroup": "scss",
+                "buildPath": "build/scss/material-design/",
+                "files": [
+                    {
+                        "destination": "_variables.scss",
+                        "format": "scss/variables"
+                    }
+                ]
+            },
+            "json": {
+                "transformGroup": "tokens-json",
+                "buildPath": "build/json/figma/",
+                "files": [
+                    {
+                        "destination": "tokens.json",
+                        "format": "json/flat"
+                    }
+                ]
+            }
         }
-        // ...
+    };
+}
+
+// REGISTER CUSTOM FORMATS
+StyleDictionaryPackage.registerFormat({
+    name: 'json/flat',
+    formatter: function (dictionary) {
+        return JSON.stringify(dictionary.allProperties, null, 2);
     }
 });
 
-StyleDictionary.buildAllPlatforms();
+// REGISTER CUSTOM TRANSFORMS
+StyleDictionaryPackage.registerTransformGroup({
+    name: 'tokens-json',
+    transforms: ["attribute/cti", "name/cti/kebab", "size/px", "color/css"]
+});
+
+
+// START BUILD
+console.log('Build started...');
+console.log('\n==============================================');
+
+const StyleDictionary = StyleDictionaryPackage.extend(getStyleDictionaryConfig());
+
+StyleDictionary.buildPlatform('json');
+StyleDictionary.buildPlatform('scss');
+
+console.log('\n==============================================');
+console.log('\nBuild completed!');
