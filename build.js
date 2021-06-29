5892663716fe251fda93de0661cb6680249e16f5
Sadwick Schuldt <sadwick.schuldt@exanic.ch>
2021-06-29 10:17:02 +0200

    Diverse Versuche für json generierung

--- build.js
+++ build.js
@@ -19,7 +19,7 @@ function getStyleDictionaryConfig() {
                 ]
             },
             "json": {
-                "transformGroup": "tokens-json",
+                "transformGroup": "tokens-json-figma",
                 "buildPath": "build/json/figma/",
                 "files": [
                     {
@@ -33,6 +33,7 @@ function getStyleDictionaryConfig() {
 }
 
 // REGISTER CUSTOM FORMATS
+// Output Format
 StyleDictionaryPackage.registerFormat({
     name: 'json/figma',
     formatter: function ({ dictionary, platform, options, file }) {
@@ -41,9 +42,45 @@ StyleDictionaryPackage.registerFormat({
 });
 
 // REGISTER CUSTOM TRANSFORMS
+// Manipulate Single Values
+StyleDictionaryPackage.registerTransform({
+    type: 'value',
+    transitive: false,
+    name: 'FigmaValue',
+    matcher: (token) => {
+        return token.value != "";
+    },
+    transformer: (token) => {
+        // token.value will be resolved and transformed at this point
+        try {
+            return "crazy-" + token.value;
+        } catch {
+            return "";
+        }
+    }
+})
+
+StyleDictionaryPackage.registerTransform({
+    type: 'name',
+    transitive: false,
+    name: 'FigmaName',
+    matcher: (token) => {
+        return token.value != "";
+    },
+    transformer: (token) => {
+        // token.value will be resolved and transformed at this point
+        try {
+            return "woho-" + token.name;
+        } catch {
+            return "";
+        }
+    }
+})
+
+// Grouping of Transforms
 StyleDictionaryPackage.registerTransformGroup({
-    name: 'tokens-json',
-    transforms: ["attribute/cti", "name/cti/kebab", "size/px", "color/css"]
+    name: 'tokens-json-figma',
+    transforms: ["attribute/cti", "name/cti/kebab", "size/px", "color/css", "FigmaValue", "FigmaName" ]
 });
 
 
